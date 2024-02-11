0327fbd0c2618b6bb38c9ae68169c40931a0239b
IkimathiG <158262018+IkimathiG@users.noreply.github.com>
2024-02-11 19:47:40 +0300

    Add files via upload

---
+++ Two.cxx
@@ -0,0 +1,21 @@
+/*introduction to programming
+additional of two numbers 
+by IkimathiG
+*/
+#include <stdio.h>
+
+int main() {
+int a,b, sum;
+
+printf("enter the first number:");
+scanf("%d",&a);
+
+printf("enter the second number:");
+scanf("%d",&b);
+
+sum= a+b;
+
+printf("The sum of%d and%d is %d",a,b,sum);
+
+    return 0; 
+    }
