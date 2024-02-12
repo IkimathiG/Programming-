936ab5e383d8eb07690e84275b0b47cfbe7323bf
IkimathiG <158262018+IkimathiG@users.noreply.github.com>
2024-02-12 21:14:11 +0300

    BMI.cxx
    BMI.cxx

---
+++ BMI.cxx
@@ -0,0 +1,20 @@
+/*C program that calculates the Body Mass Index (BMI) based on my input for weight and height
+Date Feb 12 2024
+By IkimathiG*/
+#include <stdio.h>
+
+int main() {
+    float weight, height, bmi;
+
+    printf("Enter your weight (in kilograms):\n ");
+    scanf("%f", &weight);
+
+    printf("Enter your height (in meters):\n ");
+    scanf("%f", &height);
+
+    bmi = weight / (height * height);
+
+    printf("Your BMI is: %.2f\n", bmi);
+
+    return 0;
+}
